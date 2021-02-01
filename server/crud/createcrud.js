c540bda4de3b520a6e030513351072eb0185b2c6
ucef8233 <-u>
2021-02-01 15:11:57 +0100

    test readline function

--- server/crud/createcrud.js
+++ server/crud/createcrud.js
@@ -1,5 +1,15 @@
 const { createController, createModel, createRoute, createValidation } = require('../creations/creations')
+const readline = require('readline');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What do you think of Node.js? ', async (answer) => {
+  modelName = await answer
+  rl.close();
+})
 exports.createCrud = (modelName) => {
   createController(modelName)
   createModel(modelName)
