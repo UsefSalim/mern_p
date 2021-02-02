141dc76cfa725b262d99885c76e5f0799c572b2e
ucef8233 <-u>
2021-02-02 18:56:40 +0100

    fix beu creation scripts

--- server/creations/creations.js
+++ server/creations/creations.js
@@ -94,14 +94,15 @@ exports.gitignore = () => {
 exports.jsonParce = () => {
   fs.readFile(`${stepBack}/package.json`, "utf-8", (err, data) => {
     const package = JSON.parse(data);
-    package.scripts.new = `"createController": "cd node_modules/@ucef/mernpack/server/crud && node createController",
-     "createCrud": "cd node_modules/@ucef/mernpack/server/crud && node createCrud",
-     "createModel": "cd node_modules/@ucef/mernpack/server/crud && node createModel",
-     "createRoute": "cd node_modules/@ucef/mernpack/server/crud && node createRoute",
-     "createValidation": "cd node_modules/@ucef/mernpack/server/crud && node createValidation",
-     "createPrettierConfig" : "cd node_modules/@ucef/mernpack/server/crud && node createPrettierConfig,
-     "mernPrettier" : "npm i prettier eslint eslint-config-prettier eslint-plugin-prettier eslint-plugin-import eslint-config-airbnb-base"`;
-    fs.writeFile(`${stepBack}/package.json`, JSON.stringify(package), (err) => {
+    package.scripts.createController = "cd node_modules/@ucef/mernpack/server/crud && node createController"
+    package.scripts.createCrud = "cd node_modules/@ucef/mernpack/server/crud && node createCrud"
+    package.scripts.createModel = "cd node_modules/@ucef/mernpack/server/crud && node createModel"
+    package.scripts.createRoute = "cd node_modules/@ucef/mernpack/server/crud && node createRoute"
+    package.scripts.createValidation = "cd node_modules/@ucef/mernpack/server/crud && node createValidation"
+    package.scripts.createPrettierConfig = "cd node_modules/@ucef/mernpack/server/crud && node createValidation"
+    package.scripts.mernPrettier = "npm i prettier eslint eslint-config-prettier eslint-plugin-prettier eslint-plugin-import eslint-config-airbnb-base"
+    package.scripts.dev = "nodemon server.js"
+    fs.writeFile(`${stepBack} /package.json`, JSON.stringify(package), (err) => {
       if (err) console.log("err", err);
     })
   });
